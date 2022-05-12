d3a5cfd1cbe9a6e4b07b451002f316c248afa234
Amped Scripts <52733865+AmpedScripts@users.noreply.github.com>
2022-05-12 13:04:30 +0200

    added a args check

--- compiler.js
+++ compiler.js
@@ -1,15 +1,21 @@
 var args = process.argv.slice(2);
 var fs = require('fs')
-fs.readFile(`${args}`, 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  data = data.replaceAll("amped.print(", "console.log(");
-  data = data.replaceAll("amped.log(",  'console.log("[' + new Date().toISOString().substring(11,23) + '] " +');
-  data = data.replaceAll("variable", "var");
-  data = data.replaceAll("constant", "const");
-  data = data.replaceAll("private-variable", "let");
-  data = data.replaceAll("include(", "require(");
-  data = data.replaceAll("runcode(", "eval(");
-  eval(data);
-});
+
+if (args.length < 1) {
+  console.log('Usage: node compiler.js <file>.amped');
+  process.exit(1);
+} else {
+  fs.readFile(`${args}`, 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    data = data.replaceAll("amped.print(", "console.log(");
+    data = data.replaceAll("amped.log(",  'console.log("[' + new Date().toISOString().substring(11,23) + '] " +');
+    data = data.replaceAll("variable", "var");
+    data = data.replaceAll("constant", "const");
+    data = data.replaceAll("private-variable", "let");
+    data = data.replaceAll("include(", "require(");
+    data = data.replaceAll("runcode(", "eval(");
+    eval(data);
+  });
+}
