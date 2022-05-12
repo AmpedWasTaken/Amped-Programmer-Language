3f238f52bf245980612d20e17434420d5e7de8f7
Amped Scripts <52733865+AmpedScripts@users.noreply.github.com>
2022-05-12 10:56:39 +0200

    update

--- compiler.js
+++ compiler.js
@@ -1,23 +1,3 @@
-// // 👇️ if using ES6 imports uncomment next line
-// // import {readFile, writeFile, writeFileSync, promises as fsPromises} from 'fs';
-// const {readFile, writeFile, promises: fsPromises} = require('fs');
-// var args = process.argv.slice(2);
-
-// readFile(`${args}`, 'utf-8', function (err, contents) {
-//   if (err) {
-//     console.log(err);
-//     return;
-//   }
-
-//   // 👇️ match string case-insensitively 👇️
-//   var code = contents.replace("amped.print(", "console.log(");
-//   // var code = contents.replace("amped.log(", "console.log(["+Date.now()+"] ");
-//   // eval(code);
-
-//   writeFile(`${args}`, code, 'utf-8', function (err) {
-//     console.log(err);
-//   });
-// });
 var args = process.argv.slice(2);
 var fs = require('fs')
 fs.readFile(`${args}`, 'utf8', function (err,data) {
@@ -30,11 +10,6 @@ fs.readFile(`${args}`, 'utf8', function (err,data) {
   data = data.replaceAll("constant", "const");
   data = data.replaceAll("private-variable", "let");
   data = data.replaceAll("include(", "require(");
+  data = data.replaceAll("runcode(", "eval(");
   eval(data);
-
-
-
-  // fs.writeFile(`${args}`, result, 'utf8', function (err) {
-  //    if (err) return console.log(err);
-  // });
 });
